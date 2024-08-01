7f68b1a8d3b139e3a90594d4bc8cb4c7466770f8
YJY <37117594+yen-junyu@users.noreply.github.com>
2024-08-01 23:31:36 +0800

    Create KpiMonitor.tsx

---
+++ 2408/KpiMonitor.tsx
@@ -0,0 +1,191 @@
+import { Col, Input, Row, Table, TableColumnsType } from "antd";
+import React, { useMemo } from "react";
+import styled from "styled-components";
+import ReactECharts from "echarts-for-react";
+import Chart from "./Echart";
+import HeatMap from "./HeatMap";
+import { Header } from "antd/es/layout/layout";
+import HeatmapWithDynamicAxes from "./HeatmapWithDynamicAxes";
+
+interface Props {
+  data: any[];
+}
+
+const KpiMonitor: React.FC<Props> = ({ data }: Props) => {
+  const grid = {
+    left: 100,
+    right: 100,
+    top: 50,
+    bottom: 50,
+  };
+  const rawData = [
+    [100, 302, 301, 334, 390, 330, 320],
+    [320, 132, 101, 134, 90, 230, 210],
+    [220, 182, 191, 234, 290, 330, 310],
+    [150, 212, 201, 154, 190, 330, 410],
+    [820, 832, 901, 934, 1290, 1330, 1320],
+  ];
+  const totalData: number[] = [];
+  for (let i = 0; i < rawData[0].length; ++i) {
+    let sum = 0;
+    for (let j = 0; j < rawData.length; ++j) {
+      sum += rawData[j][i];
+    }
+    totalData.push(sum);
+  }
+
+  const series = [
+    "Direct",
+    "Mail Ad",
+    "Affiliate Ad",
+    "Video Ad",
+    "Search Engine",
+  ].map((name, sid) => {
+    return {
+      name,
+      type: "bar",
+      stack: "total",
+      barWidth: "60%",
+      yAxisIndex: 0,
+
+      label: {
+        show: true,
+        formatter: (params: { value: number }) =>
+          Math.round(params.value * 1000) / 10 + "%",
+      },
+      data: rawData[sid].map((d, did) =>
+        totalData[did] <= 0 ? 0 : d / totalData[did]
+      ),
+    };
+  });
+
+  const options = {
+    grid: grid,
+    legend: {
+      selectedMode: true,
+    },
+    xAxis: [
+      {
+        type: "category",
+        data: ["1月", "2月", "3月", "4月", "5月", "6月"],
+        axisLine: { onZero: false },
+        axisTick: { alignWithLabel: true },
+        position: "bottom", // 默认位置，底部
+      },
+      // {
+      //   type: "category",
+      //   data: ["A", "B", "C", "D", "E", "F"],
+      //   axisLine: { onZero: false },
+      //   axisTick: { alignWithLabel: true },
+      //   position: "top", // 第二个 X 轴位置，顶部
+      // },
+    ],
+
+    yAxis: [
+      {
+        type: "value",
+      },
+      // {
+      //   type: "value",
+      //   name: "Temperature",
+      //   min: 0,
+      //   max: 25,
+      //   interval: 5,
+      //   axisLabel: {
+      //     formatter: "{value} °C",
+      //   },
+      // },
+    ],
+    series: [
+      ...series,
+      // {
+      //   name: "Temperature",
+      //   type: "line",
+      //   yAxisIndex: 1,
+      //   tooltip: {
+      //     valueFormatter: function (value: string) {
+      //       return value + " °C";
+      //     },
+      //   },
+      //   data: [
+      //     2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2,
+      //   ],
+      // },
+    ],
+    tooltip: {
+      trigger: "axis",
+    },
+  };
+  const onChartClick = (params: {
+    seriesType: string;
+    seriesName: any;
+    name: any;
+    value: any;
+  }) => {
+    if (params.seriesType === "bar") {
+      console.log("点击的系列:", params.seriesName);
+      console.log("点击的类目:", params.name);
+      console.log("点击的值:", params.value);
+      alert(
+        `你点击了${params.seriesName}中的${params.name}，数值是${params.value}`
+      );
+    }
+  };
+
+  // const handleLegendChange = (params: { selected: { [x: string]: boolean; }; }) => {
+  //   // 更新数据的逻辑
+  //   if (chartRef.current) {
+  //     const updatedSeries = chartData.series.map((item) => {
+  //       if (params.selected[item.name] === false) {
+  //         // 图例未选中，隐藏数据
+  //         return { ...item, data: [] };
+  //       } else {
+  //         // 图例选中，显示原数据
+  //         if (item.name === '销量') {
+  //           return { ...item, data: [5, 20, 36, 10, 10, 20] };
+  //         } else if (item.name === '利润') {
+  //           return { ...item, data: [15, 25, 26, 20, 15, 30] };
+  //         }
+  //         return item;
+  //       }
+  //     });
+  //     setChartData({ series: updatedSeries });
+  //     chartRef.current.getEchartsInstance().setOption({ series: updatedSeries });
+  //   }
+  // };
+  const handleLegendChange = (p: any) => {
+    console.log(p);
+  };
+  return (
+    <>
+      <Header style={{ background: "#fff", padding: 0 }}>
+        <Input
+          placeholder="请输入内容"
+          style={{ width: "80%", margin: "16px auto", display: "block" }}
+        />
+      </Header>
+      <Row gutter={[16, 16]} justify="center">
+        <Col
+          xs={24}
+          md={12}
+          style={{ background: "#fafafa", padding: "20px", height: "500px" }}
+        >
+          <Chart data={[]}></Chart>
+        </Col>
+        <Col xs={24} md={12} style={{ background: "#f0f0f0", padding: "20px" }}>
+        <HeatmapWithDynamicAxes></HeatmapWithDynamicAxes>
+        </Col>
+      </Row>
+      <div style={{ height: "1000px", width: "1200px" }}></div>
+
+      {/* <div style={{ height: "500px", width: "600px" }}>
+        <HeatMap data={[]}></HeatMap>
+      </div> */}
+    </>
+  );
+};
+
+export default KpiMonitor;
+
+// =============== Styled-Components ==============
+const StyledComponent = styled.div``;
